d9c2d0e42d2687b92c4cd625a7573c7b6a002430
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-04 20:08:14 -0700

    loading data from the JSON file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
+let finalOrder = {};
+let orderLineItems = {};
+let finalAmount = 0;
+
+$(document).ready(function () {
+    loadProducts();
+});
+
+function loadProducts() {
+    $.getJSON('content/books.json', function (data) {
+        let listItems = [];
+        $.each(data, function (key, val) {
+            let orderLineItem = {
+                "book": val,
+                "count": 0
+            };
+            orderLineItems[orderLineItem.book.id] = orderLineItem;
+            let listItem =
+                '<li class="list-group-item d-flex align-items-center">' +
+                '<img src = " ' + orderLineItem.book.image + ' " alt = "' + orderLineItem.book.alt + '"/> ' +
+                '<h2>' + orderLineItem.book.bookTitle + '</h2 >' +
+                '<p>' + orderLineItem.book.description + '</p >' +
+                '<button type="button" class="btn" title="' + orderLineItem.book.titlePhysical + '">' +
+                '<i class="fa fa-plus" aria-hidden="true"></i> ' + '<i class="fa fa-usd" aria-hidden="true"></i> ' +
+                '<span>' + orderLineItem.book.options.Physical + '</span>' +
+                '</button>' +
+                '<button type="button" class="btn" title="' + orderLineItem.book.titlePDF + '">' +
+                '<i class="fa fa-plus" aria-hidden="true"></i> ' + '<i class="fa fa-usd" aria-hidden="true"></i> ' +
+                '<span>' + orderLineItem.book.options.PDF + '</span>' +
+                '</button>' +
+                '</li>';
+
+            listItems.push(listItem);
+        });
+
+        $("#all-products").append(listItems.join(''));
+        // Task 2: Add the missing line. Hint: The list may need to be refreshed to reapply the styles as the list is build dynamically instead of static
+        // $("#all-products").listview('refresh');
+        // $('#all-products').trigger('change', true);
+    });
+}
+
 
