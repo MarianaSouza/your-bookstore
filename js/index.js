6666f1d2eb39a17883c0f3988b05f2fcac1dc7cf
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-06 18:19:02 -0700

    adding the selected list functionality

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-let finalOrder = {};
-let orderLineItems = {};
+let finalOrder = [];
+let allBooks = [];
 let finalAmount = 0;
-
 
 loadProducts();
 
@@ -17,26 +16,26 @@ function loadProducts() {
             console.log('error: ' + err);
         });
 
-    function appendData(data) {
+    function appendData(books) {
         let listItems = [];
-        [].forEach.call(data, function (val) {
+        books.forEach(function (book) {
             let orderLineItem = {
-                "book": val,
+                "item": book,
                 "count": 0
             };
-            orderLineItems[orderLineItem.book.id] = orderLineItem;
+            allBooks.push(orderLineItem);
             let listItem =
                 '<li class="list-group-item d-flex align-items-center">' +
-                '<img src = " ' + orderLineItem.book.image + ' " alt = "' + orderLineItem.book.alt + '"/> ' +
-                '<h2>' + orderLineItem.book.bookTitle + '</h2 >' +
-                '<p>' + orderLineItem.book.description + '</p >' +
-                '<button type="button" class="btn" title="' + orderLineItem.book.titlePhysical + '">' +
+                '<img src = " ' + book.image + ' " alt = "' + book.alt + '"/> ' +
+                '<h2>' + book.bookTitle + '</h2 >' +
+                '<p>' + book.description + '</p >' +
+                '<button type="button" class="btn" onclick="Add('+book.id+')" title="' + book.titlePhysical + '">' +
                 '<i class="fa fa-plus" aria-hidden="true"></i> ' + '<i class="fa fa-usd" aria-hidden="true"></i> ' +
-                '<span>' + orderLineItem.book.options.Physical + '</span>' +
+                '<span>' + book.options.Physical + '</span>' +
                 '</button>' +
-                '<button type="button" class="btn" title="' + orderLineItem.book.titlePDF + '">' +
+                '<button type="button" class="btn" title="' + book.titlePDF + '">' +
                 '<i class="fa fa-plus" aria-hidden="true"></i> ' +
-                '<span>' + "PDF " + '<i class="fa fa-usd" aria-hidden="true"></i> ' + orderLineItem.book.options.PDF + '</span>' +
+                '<span>' + "PDF " + '<i class="fa fa-usd" aria-hidden="true"></i> ' + book.options.PDF + '</span>' +
                 '</button>' +
                 '</li>';
 
@@ -49,7 +48,38 @@ function loadProducts() {
         element.innerHTML = listItems.join('');
         allProducts.appendChild(element);
     }
-
 }
 
+function displaySelectedList() {
+    let listSelectedItems = [];
+    allBooks.forEach(function (orderLineItem) {
+        console.log("Order", orderLineItem);
+        if(orderLineItem.count == 0) return;
+        let book = orderLineItem.item;
+        let listSelectedItem =
+            '<li class="list-group-item d-flex align-items-center">' +
+            '<img src=" ' + book.image + ' " alt = "' + book.alt + '"/>' +
+            '<h2>' + book.bookTitle + '</h2>' +
+            '<p>' + orderLineItem.count + '</p>' +
+            '<button id="btn_' + book.id + '_add" onclick="Remove(this)" type="button" class="btn"><strong>Remove</strong></button>' +
+            '</li>';
 
+        listSelectedItems.push(listSelectedItem);
+    })
+    let selectedProducts = document.getElementById("selected-products-div");
+    while(selectedProducts.firstChild) selectedProducts.removeChild(selectedProducts.firstChild);
+    let element = document.createElement('ul');
+    element.classList.add("list-group")
+    element.innerHTML = listSelectedItems.join('');
+    selectedProducts.appendChild(element);
+   
+}
+
+function Add(id){
+    allBooks.forEach(book => {
+        if(id == book.item.id){
+            book.count = book.count + 1;  
+        }
+    })
+    displaySelectedList()
+}
